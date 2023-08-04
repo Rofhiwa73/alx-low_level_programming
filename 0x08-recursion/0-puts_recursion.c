229f0a40380884392c585b8ffd92b1506e54b365
Henessy <125590551+Rofhiwa73@users.noreply.github.com>
2023-08-04 11:42:24 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Function that prints a string.
+ *
+ * @s: String to be printed.
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
